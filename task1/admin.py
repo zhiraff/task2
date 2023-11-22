ab48f3f9f887a429348de982a22cff8b5dc5041d
Timofey Valerevich <zhiraph@mail.ru>
2023-11-22 14:35:20 +0700

    add googledrive

--- task1/admin.py
+++ task1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import MyFiles
+
+#class MyFilesAdmin(admin.ModelAdmin):
+#    list_display = ['id', 'file_name', 'file_data']
+#
+#admin.site.register(MyFiles, MyFilesAdmin)
+
+admin.site.register(MyFiles)
